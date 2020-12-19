6c58266caf9711aa7607b045903a2e569395d60d
Bennett Sewell <bennettgsewell@gmail.com>
2020-12-19 13:46:40 -0500

    Added the most recent version of the project I have, 1/30/2016

---
+++ Assets/Rite_ot_Dawn/Scripts/Parallax.cs
@@ -0,0 +1,31 @@
+﻿//Written by Bennett Sewell
+//Parallax script for the background stuff
+
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Parallax : MonoBehaviour {
+    public float mult = 1f;
+    public float midPoint = 0f;
+    public float farPoint = 5f;
+    public Transform ground;
+    public Transform targetPivot;
+    public Transform[] parObjects;
+    float x;
+    public void Update()
+    {
+        foreach (Transform t in parObjects)
+        x = (targetPivot.position.x - midPoint) / farPoint;
+        foreach (Transform t in parObjects)
+        {
+            t.localScale = new Vector3(Mathf.Abs(t.position.z), Mathf.Abs(t.position.z), 1f);
+            t.position = new Vector3(midPoint + x * mult * t.position.z * -1f, t.position.y, t.position.z);
+        }
+    }
+
+    public void OnGUI()
+    {
+        Update();
+    }
+}
